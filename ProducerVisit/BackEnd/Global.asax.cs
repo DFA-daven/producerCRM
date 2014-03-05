ce05891a8b6e67b7eaac071594b5266c2636f516
dave.nouza <dave.nouza@4b544b15-6f6e-364e-8cc7-e426d1e3f8bb>
2014-03-05 19:28:33 +0000

    checking in a few things that shouldn't impact project
    git-svn-id: http://dl-version-01/svn/Dairylea%20Source/Mobile@3058 4b544b15-6f6e-364e-8cc7-e426d1e3f8bb

--- ProducerVisit/BackEnd/Global.asax.cs
+++ ProducerVisit/BackEnd/Global.asax.cs
@@ -1,24 +1,13 @@
-﻿using System;
-using BackEnd.Models;
-using System.Data.Entity;
-using System.Web.Http;
-using System.Web.Mvc;
-using System.Web.Optimization;
-using System.Web.Routing;
+﻿using BackEnd.Models;
 
 namespace BackEnd
 {
-<<<<<<< .mine
-=======
-    using System;
-    //using BackEnd.Models;
     using System.Data.Entity;
     using System.Web.Http;
     using System.Web.Mvc;
     using System.Web.Optimization;
     using System.Web.Routing;
 
->>>>>>> .r3056
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
 
@@ -28,24 +17,15 @@ namespace BackEnd
         {
             AreaRegistration.RegisterAllAreas();
 
-            try
-            {
-                // broken: this line must be commented out on the initial Publish/Deploy. can this be automated?
-                // Doing so enables the tables (schema) to be established in the database.
-                // After the first run, the uncommented line allows everything to work.
-                Database.SetInitializer(new DropCreateDatabaseIfModelChanges<VisitContext>());
+            // broken: this line must be commented out on the initial Publish/Deploy. can this be automated?
+            // Doing so enables the tables (schema) to be established in the database.
+            // After the first run, the uncommented line allows everything to work.
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<VisitContext>());
 
-                WebApiConfig.Register(GlobalConfiguration.Configuration);
-                FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-                RouteConfig.RegisterRoutes(RouteTable.Routes);
-                BundleConfig.RegisterBundles(BundleTable.Bundles);
-            }
-            catch (Exception exc)
-            {
-                Console.WriteLine(exc.Message);
-                Console.WriteLine(exc.StackTrace);
-            }
-            
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
