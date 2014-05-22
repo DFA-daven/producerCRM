dc2b85769cd109e9ae304d7f83674b69ea4e4ea4
dave.nouza <dave.nouza@4b544b15-6f6e-364e-8cc7-e426d1e3f8bb>
2014-05-22 18:36:33 +0000

    adding favicon.ico
    git-svn-id: http://dl-version-01/svn/Dairylea%20Source/Mobile@3159 4b544b15-6f6e-364e-8cc7-e426d1e3f8bb

--- ProducerVisit/AutoVersionNumber/GlobalAssemblyVersion.cs
+++ ProducerVisit/AutoVersionNumber/GlobalAssemblyVersion.cs
@@ -19,8 +19,8 @@ using System.Runtime.InteropServices;
 // Major and minor version are hard-coded.
 // Build number is: [two-digit year][three-digit day-of-year]
 // Revision is: the number of seconds since midnight (divided by 5)
-[assembly: AssemblyVersion("1.4.14140.12024")]
-[assembly: AssemblyFileVersion("1.4.14140.12024")]
+[assembly: AssemblyVersion("1.4.14141.12705")]
+[assembly: AssemblyFileVersion("1.4.14141.12705")]
 
 [assembly: AssemblyProduct("ProducerCRM")]
 [assembly: AssemblyCompany("Dairylea Cooperative, Inc.")]
