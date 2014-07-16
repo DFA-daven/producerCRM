4c362bcd53669e060709860ed83790be906ee88f
dave.nouza <dave.nouza@4b544b15-6f6e-364e-8cc7-e426d1e3f8bb>
2014-07-16 15:44:05 +0000

    version number, etc.
    git-svn-id: http://dl-version-01/svn/Dairylea%20Source/Mobile@3333 4b544b15-6f6e-364e-8cc7-e426d1e3f8bb

--- ProducerVisit/AutoVersionNumber/GlobalAssemblyVersion.cs
+++ ProducerVisit/AutoVersionNumber/GlobalAssemblyVersion.cs
@@ -19,8 +19,8 @@ using System.Runtime.InteropServices;
 // Major and minor version are hard-coded.
 // Build number is: [two-digit year][three-digit day-of-year]
 // Revision is: the number of seconds since midnight (divided by 5)
-[assembly: AssemblyVersion("1.4.14195.13498")]
-[assembly: AssemblyFileVersion("1.4.14195.13498")]
+[assembly: AssemblyVersion("1.4.14196.12977")]
+[assembly: AssemblyFileVersion("1.4.14196.12977")]
 
 [assembly: AssemblyProduct("ProducerCRM")]
 [assembly: AssemblyCompany("Dairylea Cooperative, Inc.")]
