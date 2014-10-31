ed9fbecdc2ce694bf4855ad39b9a5f35e56a9094
dave.nouza <dave.nouza@4b544b15-6f6e-364e-8cc7-e426d1e3f8bb>
2014-10-31 21:04:57 +0000

    Got a connection to the Build Host!
    git-svn-id: http://dl-version-01/svn/Dairylea%20Source/Mobile@3487 4b544b15-6f6e-364e-8cc7-e426d1e3f8bb

--- ProducerVisit/AutoVersionNumber/GlobalAssemblyVersion.cs
+++ ProducerVisit/AutoVersionNumber/GlobalAssemblyVersion.cs
@@ -19,8 +19,13 @@ using System.Runtime.InteropServices;
 // Major and minor version are hard-coded.
 // Build number is: [two-digit year][three-digit day-of-year]
 // Revision is: the number of seconds since midnight (divided by 5)
+<<<<<<< .mine
+[assembly: AssemblyVersion("1.4.14302.12871")]
+[assembly: AssemblyFileVersion("1.4.14302.12871")]
+=======
 [assembly: AssemblyVersion("1.4.14301.9355")]
 [assembly: AssemblyFileVersion("1.4.14301.9355")]
+>>>>>>> .r3486
 
 [assembly: AssemblyProduct("ProducerCRM")]
 [assembly: AssemblyCompany("Dairylea Cooperative, Inc.")]
